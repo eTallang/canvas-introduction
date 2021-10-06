98b056be00a267d2662ece80e45098bbafb1cb9a
Erik Tallang <e.tallang@gmail.com>
2021-10-06 21:44:49 +0200

    feat: clear canvas for each render

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import Circle from './circle';
 
 const container = document.getElementById('root') as HTMLCanvasElement;
 const context = container.getContext('2d');
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
 
-container.height = window.innerHeight;
-container.width = window.innerWidth;
+container.height = canvasHeight;
+container.width = canvasWidth;
 container.style.backgroundColor = 'rgba(16, 18, 25, 1)';
 
 const circle = new Circle(context);
 
 function render() {
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
   circle.update();
   window.requestAnimationFrame(render);
 }
