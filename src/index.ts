b5e539fb963d0c1870cf473baa515c7ce601b377
Erik Tallang <e.tallang@gmail.com>
2021-10-06 21:21:56 +0200

    feat: draw a circle

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ const context = container.getContext('2d');
 container.height = window.innerHeight;
 container.width = window.innerWidth;
 container.style.backgroundColor = 'rgba(16, 18, 25, 1)';
+
+context.beginPath();
+context.arc(400, 400, 100, 0, Math.PI * 2);
+context.closePath();
+context.fillStyle = 'white';
+context.fill();
