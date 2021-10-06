6c29379659768d939bbbf7d15be7684b992e9134
Erik Tallang <e.tallang@gmail.com>
2021-10-06 22:03:04 +0200

    feat: randomize circle values

--- src/circle.ts
+++ src/circle.ts
@@ -1,16 +1,14 @@
 export default class Circle {
-  private x = 400;
-  private dx = 5;
-
-  private y = 400;
-  private dy = 5;
-
-  private radius = 100;
-
   constructor(
     private context: CanvasRenderingContext2D,
     private canvasWidth: number,
-    private canvasHeight: number
+    private canvasHeight: number,
+    private color: string,
+    private x: number,
+    private y: number,
+    private dx: number,
+    private dy: number,
+    private radius: number
   ) {}
 
   private updatePosition() {
@@ -33,7 +31,7 @@ export default class Circle {
     this.context.beginPath();
     this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     this.context.closePath();
-    this.context.fillStyle = 'white';
+    this.context.fillStyle = this.color;
     this.context.fill();
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import '../styles.scss';
 import Circle from './circle';
 
+const colors = ['#fffce0', '#D94862', '#F26849'];
 const container = document.getElementById('root') as HTMLCanvasElement;
 const context = container.getContext('2d');
 const canvasWidth = window.innerWidth;
@@ -16,16 +17,34 @@ function createCircles(amount: number) {
   circles = [];
 
   for (let i = 0; i < amount; i++) {
-    const circle = new Circle(context, canvasWidth, canvasHeight);
+    const size = Math.random();
+    const radius = size * 5;
+    const x = Math.random() * (canvasWidth - radius * 2) + radius;
+    const y = Math.random() * (canvasHeight - radius * 2) + radius;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const dx = Math.random() * size * 2 * (3 - 6 * Math.round(Math.random()));
+    const dy = Math.random() * size * 2 * (3 - 6 * Math.round(Math.random()));
+
+    const circle = new Circle(
+      context,
+      canvasWidth,
+      canvasHeight,
+      color,
+      x,
+      y,
+      dx,
+      dy,
+      radius
+    );
     circles.push(circle);
   }
 }
 
 function render() {
   context.clearRect(0, 0, canvasWidth, canvasHeight);
-  circles.forEach(circle => circle.update());
+  circles.forEach((circle) => circle.update());
   window.requestAnimationFrame(render);
 }
 
-createCircles(10);
+createCircles(400);
 render();
